b6504f15c55524ca9ad8a0ab864dfcf13d86f7ad
anifa <anifabazubagira@gmail.com>
2025-02-25 11:38:26 +0200

    finding areas of given shapes

---
+++ shapeArea/shape.js
@@ -0,0 +1,8 @@
+function circleArea(radius){
+    return 3.14 * radius * radius
+}
+function squareArea(length){
+    return length * length
+}
+module.exports.circleArea = circleArea;
+module.exports.squareArea = squareArea;
